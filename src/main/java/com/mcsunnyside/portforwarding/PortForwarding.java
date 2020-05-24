1225ea834ad00f7400e9f888f49941de16a747b3
Ghost_chu <2908803755@qq.com>
2020-05-24 23:05:05 +0800

    Commit

---
+++ src/main/java/com/mcsunnyside/portforwarding/PortForwarding.java
@@ -0,0 +1,73 @@
+package com.mcsunnyside.portforwarding;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PortForwarding extends JavaPlugin {
+    private Session session;
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        saveDefaultConfig();
+        JSch jSch = new JSch();
+        try {
+            jSch.addIdentity(getConfig().getString("private-key"),getConfig().getString("passphrase"));
+            session = jSch.getSession(getConfig().getString("ssh-usr"),getConfig().getString("ssh-host"),getConfig().getInt("ssh-port"));
+            session.setHostKeyRepository(jSch.getHostKeyRepository());
+            if(getConfig().getString("ssh-pwd") != null){
+                session.setPassword(getConfig().getString("ssh-pwd"));
+            }
+            getLogger().info("Connecting to remote SSH server...");
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+            if(!session.isConnected()){
+                getLogger().warning("Failed connect to remote SSH server...");
+                return;
+            }else{
+                getLogger().info("Successfully connect to remote SSH server!");
+            }
+
+            getLogger().info("Setting up forwarding...");
+            getConfig().getStringList("rules").forEach((rule)->{
+                //LOCAL PORT:REMOTE PORT
+                String[] parameters = rule.split(":");
+                if(parameters.length != 2){
+                    getLogger().warning("Invalid rule: "+rule);
+                    return;
+                }
+                try {
+                    //session.setPortForwardingR(Integer.parseInt(parameters[1]),"localhost",Integer.parseInt(parameters[0]));
+                    session.setPortForwardingR(null,Integer.parseInt(parameters[1]),"127.0.0.1",Integer.parseInt(parameters[0]));
+                    getLogger().info("Successfully setup rule: "+rule);
+                } catch (JSchException e) {
+                    e.printStackTrace();
+                    getLogger().warning("Failed to setup rule: "+rule);
+                }
+            });
+        } catch (JSchException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getConfig().getStringList("rules").forEach((rule)->{
+            //LOCAL PORT:REMOTE PORT
+            String[] parameters = rule.split(":");
+            if(parameters.length != 2){
+                getLogger().warning("Invalid rule: "+rule);
+                return;
+            }
+            try {
+                session.delPortForwardingR(Integer.parseInt(parameters[0]));
+                session.delPortForwardingR(Integer.parseInt(parameters[1]));
+            } catch (JSchException ignored) {
+            }
+        });
+        session.disconnect();
+    }
+}
